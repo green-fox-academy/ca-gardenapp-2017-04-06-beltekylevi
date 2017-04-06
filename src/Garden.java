db2609d64b48f37e3d0805b64478f4f40e8881f3
Levente Belteky <belteky.levente@gmail.com>
2017-04-06 11:09:46 +0200

    feat(app): add garden model

---
+++ src/Garden.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garden {
+    List<Plant> plants;
+
+    public Garden() {
+        this.plants = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+
+        for (Plant plant : plants) {
+            result += plant.toString() + "\n";
+        }
+
+        return result;
+    }
+}
